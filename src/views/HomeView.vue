39dbc1860efa481644abaf8b4f6f36269b083c0e
YL-5001 <2546091034@qq.com>
2024-01-15 23:43:42 +0800

    首页样式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <template>
   <main>
     <div class="web_home">
-      <h1>Hello</h1>
+      <header>
+        <div class="background_dorder">
+          <h1>Hello</h1>
+        </div>
+      </header>
     </div>
     
     <TheWelcome />
@@ -13,9 +17,29 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <style lang="scss" scoped>
-  .web_home{
+  .web_home {
     width: 100%;
     height: 30vh;
-    background-color: $web_main_color;
+    // background-color: $web_main_color;
+    header {
+      display: flex;
+      height: 100%;
+      .background_dorder {
+        display: flex;
+        margin: auto;
+        width: 60vw;
+        height: 80%;
+        border: 1px solid $web_main_color;
+
+        h1 {
+          margin: auto auto 0 auto;
+
+          line-height: 1;//使字体块高度 = 字体本身高度
+          font-size: 4em;
+          font-weight: 600;
+        }
+      }
+    }
+    
   }
 </style>
